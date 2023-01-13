35dd5598a05f4add457641e6e8c818c66d27d8b0
noah-brunate <noahbrunate9@gmail.com>
2023-01-13 07:20:40 -0800

    ssh scripting in bash and puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# puppet script to create an ssh config file
+file_line { 'Turn off password auth':
+     ensure => 'present',
+     path   => '/etc/ssh/ssh_config',
+     line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+     ensure => 'present',
+     path   => '/etc/ssh/ssh_config',
+     line   => 'IdentityFile ~/.ssh/school',
+}
